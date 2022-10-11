e2b3b97a5d6e993c82588f498222720172cf5356
Toki Urata <tokiurata@outlook.com>
2022-10-11 15:03:46 -0400

    Create typed selector

--- src/components/ItemsList.tsx
+++ src/components/ItemsList.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import { useTypedSelector } from "../hooks/useTypedSelector";
 import { useActions } from "../hooks/useActions";
 
 const ItemsList: React.FC = () => {
   const [term, setTerm] = useState('');
   const { searchItems } = useActions();
+  const { data, error, loading } = useTypedSelector((state) => state.items);
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();

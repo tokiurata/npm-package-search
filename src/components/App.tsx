dae8a29f8abad5cf6f90da7c533a680816c77679
Toki Urata <tokiurata@outlook.com>
2022-10-11 17:36:32 -0400

    Add initial styles

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,11 +2,14 @@ import { Provider } from 'react-redux';
 import { store } from '../state';
 import ItemsList from './ItemsList';
 
+import '../styles/main.scss';
+
 const App = () => {
   return (
     <Provider store={store}>
-      <div>
-        <h1>Search For a Package</h1>
+      <div className='app'>
+        <h1>npm Package Search</h1>
+
         <ItemsList />
       </div>
     </Provider>

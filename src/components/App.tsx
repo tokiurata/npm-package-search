3ee4a42fa70b196700f03309a83f868bfc0a0519
Toki Urata <tokiurata@outlook.com>
2022-10-11 12:05:31 -0400

    Connect redux to react

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,15 @@
+import { Provider } from 'react-redux';
+import { store } from '../state';
+import ItemsList from './ItemsList';
+
 const App = () => {
   return (
-    <div>App</div>
+    <Provider store={store}>
+      <div>
+        <h1>Search For a Package</h1>
+        <ItemsList />
+      </div>
+    </Provider>
   );
 };
 
---
+++ src/components/ItemsList.tsx
@@ -0,0 +1,12 @@
+const ItemsList: React.FC = () => {
+  return (
+    <div>
+      <form>
+        <input />
+        <button>Search</button>
+      </form>
+    </div>
+  )
+};
+
+export default ItemsList;

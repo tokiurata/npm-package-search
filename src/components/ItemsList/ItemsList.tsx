ece59fd1e851a447707fe7ebe3686c89149ff49b
Toki Urata <tokiurata@outlook.com>
2022-10-16 08:53:48 -0400

    Fix ItemsList

--- src/components/ItemsList/ItemsList.tsx
+++ src/components/ItemsList/ItemsList.tsx
@@ -17,23 +17,25 @@ const ItemsList: React.FC = () => {
   listStatus();
 
   return (
-    <ul className="items-list">
-      {data.objects?.map((item: any, index: number) => {
-        return (
-          <li className="items-list__item" key={index}>
-            <a className="items-list__link" href={item.package.links.npm}>
-              <h2 className="items-list__title">{item.package.name}</h2>
-            </a>
-            <p className="items-list__description">{item.package.description}</p>
-            <ul className="items-list__keyword-list">
-              {item.package.keywords?.map((keyword: string, index: number) => {
-                return <li className="items-list__keyword" key={index}>{keyword}</li>;
-              })}
-            </ul>
-          </li>
-        );
-      })}
-    </ul>
+    <div className="items-list">
+      <ul className="items-list__items">
+        {data.objects?.map((item: any, index: number) => {
+          return (
+            <li className="items-list__item" key={index}>
+              <a className="items-list__link" href={item.package.links.npm}>
+                <h2 className="items-list__title">{item.package.name}</h2>
+              </a>
+              <p className="items-list__description">{item.package.description}</p>
+              <ul className="items-list__keyword-list">
+                {item.package.keywords?.map((keyword: string, index: number) => {
+                  return <li className="items-list__keyword" key={index}>{keyword}</li>;
+                })}
+              </ul>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
